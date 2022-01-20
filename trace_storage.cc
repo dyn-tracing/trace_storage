5d496389a52126f7c9eb4c32c11fcae4cb817d86
Jessica Berg <jesswberg@gmail.com>
2022-01-20 14:30:21 -0500

    linter

--- trace_storage.cc
+++ trace_storage.cc
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "google/cloud/storage/client.h"
 #include <iostream>
+#include "google/cloud/storage/client.h"
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
