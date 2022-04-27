01e01b3a24277d0b4264ca5a89ab4086b5963d34
Jessica Berg <jesswberg@gmail.com>
2022-04-27 11:16:38 -0400

    bazel now uses boost

--- trace_storage.cc
+++ trace_storage.cc
@@ -13,12 +13,24 @@
 // limitations under the License.
 
 #include "google/cloud/storage/client.h"
-#include "opentelemetry-proto/gen/cpp/opentelemetry/proto/trace/v1/trace.pb.h"
+//#include "opentelemetry-proto/gen/cpp/opentelemetry/proto/trace/v1/trace.pb.h"
 #include <iostream>
+#include <regex>
+#include <boost/regex.hpp>
+#include <boost/algorithm/string/regex.hpp>
+
+
 
 const std::string trace_struct_bucket = "dyntraces-snicket2";
 // Create aliases to make the code easier to read.
 namespace gcs = ::google::cloud::storage;
+
+std::vector<std::string> split_string(const std::string& str)
+{
+    std::vector<std::string> tokens;
+    split_regex(tokens, str, boost::regex("(\n)+"));
+    return tokens;
+}
 
 // Gets a trace by trace ID and given timespan
 int get_trace(std::string traceID, int start_time, int end_time, gcs::Client* client) {
@@ -44,11 +56,18 @@ int get_trace(std::string traceID, int start_time, int end_time, gcs::Client* cl
             int traceID_location = contents.find(traceID);
             if (traceID_location) {
                 trace_found = true;
-                int end = contents.find("Trace ID", traceID_location);
+                int end = contents.find("Trace ID", traceID_location-1);
                 if (end) {
-                    std::string spans = contents.substr(traceID_location, end);
+                    std::string spans = contents.substr(traceID_location, end-traceID_location);
                     std::cout << spans << std::endl;
-                    std::cout << "trace id loc " << traceID_location << " end " << end << std::endl;
+                    std::vector<std::string> split_spans = split_string(spans);
+                    // start at 1 because first line will be trace ID
+                    for (int k = 1; k < split_spans.size(); k++) {
+                        std::cout << "token " << split_spans[k] << std::endl;
+
+                    }
+
+
                 }
             }
             
