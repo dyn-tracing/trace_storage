1e7dc6f8d4c5b5b17d7e319971dbb55ad8edd357
jessberg <jessberg@users.noreply.github.com>
2022-04-28 15:12:46 -0400

    Copy quickstart (#3)
    * basic trace retrieval

--- trace_storage.cc
+++ trace_storage.cc
@@ -13,93 +13,138 @@
 // limitations under the License.
 
 #include <iostream>
-#include <string>
+#include <stdexcept>
+#include <regex>
 #include "google/cloud/storage/client.h"
+#include "opentelemetry/proto/trace/v1/trace.pb.h"
+#include <boost/regex.hpp>
+#include <boost/algorithm/string/regex.hpp>
 
+const char trace_struct_bucket[] = "dyntraces-snicket3";
+const char ending[] = "-snicket3";
+// Create aliases to make the code easier to read.
 namespace gcs = ::google::cloud::storage;
 
-int write_object(gcs::Client &client, std::string bucket_name, std::string object_name, std::string content) {
-    auto writer = client.WriteObject(bucket_name, object_name);
-    writer << content;
-    writer.Close();
-    if (!writer.metadata()) {
-      std::cerr << "Error creating object: " << writer.metadata().status()
+std::vector<std::string> split_string_by_newline(const std::string& str) {
+    std::vector<std::string> tokens;
+    split_regex(tokens, str, boost::regex("(\n)+"));
+    return tokens;
+}
+
+std::vector<std::string> split_string_by_colon(const std::string& str) {
+    std::vector<std::string> tokens;
+    split_regex(tokens, str, boost::regex("(:)+"));
+    return tokens;
+}
+
+// https://codereview.stackexchange.com/questions/78535/converting-array-of-bytes-to-the-hex-string-representation
+constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+std::string hex_str(std::string data, int len) {
+  std::string s(len * 2, ' ');
+  for (int i = 0; i < len; ++i) {
+    s[2 * i]     = hexmap[(data[i] & 0xF0) >> 4];
+    s[2 * i + 1] = hexmap[data[i] & 0x0F];
+  }
+  return s;
+}
+
+
+opentelemetry::proto::trace::v1::Span get_span(
+    int hash1, int hash2, std::string microservice, std::string start_time,
+    std::string end_time, gcs::Client* client, std::string span_id) {
+    std::string obj_name = std::to_string(hash1) + std::to_string(hash2);
+    obj_name = obj_name + "-" + start_time + "-" + end_time;
+    auto reader = client->ReadObject(microservice+ending, obj_name);
+    if (reader.status().code() == ::google::cloud::StatusCode::kNotFound) {
+        std::cerr << "span object not found " << obj_name <<
+            "in microservice " << microservice << std::endl;
+        throw std::runtime_error("span object not found");
+    } else if (!reader) {
+        std::cerr << "Error reading object: " << reader.status() << "\n";
+        throw std::runtime_error("Error reading trace object");
+    } else {
+        std::string contents{std::istreambuf_iterator<char>{reader}, {}};
+        opentelemetry::proto::trace::v1::TracesData trace_data;
+        bool ret = trace_data.ParseFromString(contents);
+        if (!ret) {
+            std::cerr << " not parsed! " << std::endl;
+            throw std::runtime_error("could not parse span data");
+        }
+        int sp_size = trace_data.resource_spans(0).scope_spans(0).spans_size();
+        for (int i=0; i < sp_size; i++) {
+            opentelemetry::proto::trace::v1::Span sp =
+                trace_data.resource_spans(0).scope_spans(0).spans(i);
+            if (hex_str(sp.span_id(), sp.span_id().length()) == span_id) {
+                return sp;
+            }
+        }
+    }
+    std::cerr << "did not find span object " << span_id << std::endl;
+    throw std::runtime_error("did not find span object");
+}
+
+// Gets a trace by trace ID and given timespan
+int get_trace(std::string traceID, int start_time,
+              int end_time, gcs::Client* client) {
+    bool trace_found = false;
+    for (int i=0; i < 10; i++) {
+        if (trace_found) {
+            break;
+        }
+        for (int j=0; j < 10; j++) {
+          if (trace_found) {
+            break;
+          }
+          std::string obj_name = std::to_string(i) + std::to_string(j) + "-";
+          obj_name += std::to_string(start_time) + "-" +
+          obj_name += std::to_string(end_time);
+          auto reader = client->ReadObject(trace_struct_bucket, obj_name);
+          if (reader.status().code() ==
+            ::google::cloud::StatusCode::kNotFound) {
+            continue;
+          } else if (!reader) {
+            std::cerr << "Error reading trace object: " << reader.status()
                 << "\n";
-      return 1;
+            throw std::runtime_error("error reading trace object");
+            return 1;
+          } else {
+            std::string contents{std::istreambuf_iterator<char>{reader}, {}};
+            int traceID_location = contents.find(traceID);
+            if (traceID_location) {
+                trace_found = true;
+                int end = contents.find("Trace ID", traceID_location-1);
+                if (end) {
+                    std::string spans = contents.substr(
+                        traceID_location, end-traceID_location);
+                    std::vector<std::string> split_spans;
+                    split_spans = split_string_by_newline(spans);
+                    // start at 1 because first line will be trace ID
+                    for (int k = 1; k < split_spans.size(); k++) {
+                        if (split_spans[k] != "") {
+                            std::vector<std::string> span_info;
+                            span_info = split_string_by_colon(split_spans[k]);
+                            opentelemetry::proto::trace::v1::Span sp;
+                            sp = get_span(i, j, span_info[2],
+                                std::to_string(start_time),
+                                std::to_string(end_time), client, span_info[1]);
+                        }
+                    }
+
+
+                } else { throw std::runtime_error("couldn't find trace ID"); }
+            }
+          }
+        }
     }
     return 0;
 }
 
-std::string read_object(gcs::Client &client, std::string bucket_name, std::string object_name) {
-  auto reader = client.ReadObject(bucket_name, object_name);
-  if (!reader) {
-    std::cerr << "Error reading object: " << reader.status() << "\n";
-  }
-
-  std::string contents{std::istreambuf_iterator<char>{reader}, {}};
-  std::cout << contents << "\n";
-  return contents;
-}
-
-int delete_object(gcs::Client &client, std::string bucket_name, std::string object_name) {
-  google::cloud::Status status =
-        client.DeleteObject(bucket_name, object_name);
-  if (!status.ok()) {
-    std::cerr << "Error deleting object: " << status << "\n";
-  }
-  return 0;
-}
-
-int create_bucket(gcs::Client &client, std::string bucket_name, std::string project_id) {
-  google::cloud::StatusOr<gcs::BucketMetadata> bucket_metadata =
-      client.CreateBucketForProject(
-          bucket_name, project_id,
-          gcs::BucketMetadata()
-              .set_location("us-east1")
-              .set_storage_class(gcs::storage_class::Regional()));
-  if (!bucket_metadata) {
-    std::cerr << "Error creating bucket " << bucket_name
-              << ", status=" << bucket_metadata.status() << "\n";
-    return 1;
-  }
-  return 0;
-
-}
-
-int delete_bucket(gcs::Client &client, std::string bucket_name) {
-  google::cloud::Status status = client.DeleteBucket(bucket_name);
-  if (!status.ok()) {
-    throw std::runtime_error(status.message());
-  }
-  return 0;
-}
-
-
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    std::cerr << "Missing bucket name.\n";
-    std::cerr << "Usage: quickstart <bucket-name>\n";
-    return 1;
-  }
-  std::string const bucket_name = argv[1];
-
-  // Create aliases to make the code easier to read.
-
-  // Create a client to communicate with Google Cloud Storage. This client
-  // uses the default configuration for authentication and project id.
-  auto client = gcs::Client();
-  create_bucket(client, "trace_storage_bucket", "dynamic-tracing");
-  write_object(client, "trace_storage_bucket", "new_object", "Here is a sentence"); 
-  std::string written = read_object(client, "trace_storage_bucket", "new_object");
-  if (written == "Here is a sentence") {
-    std::cout << "Got correct bucket data" << std::endl;
-  } else {
-    std::cout << "Bucket data was instead " << written << std::endl;
-  }
-  delete_object(client, "trace_storage_bucket", "new_object");
-  delete_bucket(client, "trace_storage_bucket");
-  std::cout << "All actions done" << std::endl;
-
-
-  return 0;
+    // Create a client to communicate with Google Cloud Storage. This client
+    // uses the default configuration for authentication and project id.
+    auto client = gcs::Client();
+    return get_trace("366ada8fbc705fbddf0468d1df1e746f",
+                     1651073970, 1651073970, &client);
 }
