aca3458b54db31f664ef9555835e1f20bcd67180
Jessica Berg <jesswberg@gmail.com>
2022-05-09 16:56:30 -0400

    more testing

--- id_index/id_index_test.cc
+++ id_index/id_index_test.cc
@@ -3,7 +3,7 @@
 
 //#include <id_index_lib>
 // Demonstrate some basic assertions.
-TEST(Prefixes, TestGeneratePrefixes) {
+TEST(Prefixes, TestGeneratePrefixesSmall) {
   // Expect two strings not to be equal.
   time_t begin = 1651696900;
   time_t end = 1651697000;
@@ -13,5 +13,19 @@ TEST(Prefixes, TestGeneratePrefixes) {
   expect.push_back("16517");
   EXPECT_TRUE(std::find(prefixes.begin(), prefixes.end(), expect[0])!=prefixes.end());
   EXPECT_TRUE(std::find(prefixes.begin(), prefixes.end(), expect[1])!=prefixes.end());
+}
+
+TEST(Prefixes, TestGeneratePrefixesBig) {
+  // Expect two strings not to be equal.
+  time_t begin = 1651691111;
+  time_t end = 1651697000;
+  std::vector<std::string> prefixes = generate_prefixes(begin, end);
+  std::vector<std::string> expect;
+  for (int i=1; i<7; i++) {
+    expect.push_back("1651" + std::to_string(i));
+  }
+  for (int i=0; i<expect.size(); i++) {
+    EXPECT_TRUE(std::find(prefixes.begin(), prefixes.end(), expect[i])!=prefixes.end());
+  }
 }
 
