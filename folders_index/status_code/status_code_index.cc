7f3068972fb788b595ca76dfc8c9c1125cc2268e
Muhammad Haseeb <mh6218@nyu.edu>
2022-05-10 19:26:16 -0400

    partially implemented

--- folders_index/status_code/status_code_index.cc
+++ folders_index/status_code/status_code_index.cc
@@ -16,13 +16,13 @@ int update_index(gcs::Client* client, time_t last_updated, std::string index_sta
 	 * TODO: (i) Following is a bad thing to do. what if all object names do not fit in the memory. 
 	 * (ii) do the error handling for the case when bucket is not present. 
 	 */
-	std::vector<std::string> trace_struct_object_names = get_all_object_names(TRACE_STRUCT_BUCKET);
+	std::vector<std::string> trace_struct_object_names = get_all_object_names(TRACE_STRUCT_BUCKET, client);
 	trace_struct_object_names = sort_object_names_on_start_time(trace_struct_object_names);
 	index_batch current_index_batch = index_batch();
 
 	for (auto object_name : trace_struct_object_names) {
-		std::vector<std::string> trace_ids_with_index_status_code = get_trace_ids_with_index_status_code(object_name, index_status_code);
-		
+		std::vector<std::string> trace_ids_with_index_status_code = get_trace_ids_with_index_status_code(object_name, index_status_code, client);
+
 		current_index_batch.total_trace_ids += trace_ids_with_index_status_code.size();
 		current_index_batch.trace_ids_with_timestamps.push_back(std::make_pair(
 			extract_batch_timestamps(object_name), trace_ids_with_index_status_code
@@ -41,16 +41,77 @@ int update_index(gcs::Client* client, time_t last_updated, std::string index_sta
 	return 0;
 }
 
-std::vector<std::string> get_all_object_names(std::string bucket_name) {
-	// TODO
+std::vector<std::string> get_all_object_names(std::string bucket_name, gcs::Client* client) {
+	std::vector<std::string> response;
+
+	for (auto&& object_metadata : client->ListObjects(bucket_name)) {
+		if (!object_metadata) {
+			std::cerr << object_metadata.status().message() << std::endl;
+			exit(1);
+		}
+
+		response.push_back(object_metadata->name());
+	}
+
+	return response;
+}
+
+std::vector<std::string> split_by_char(std::string input, std::string splitter) {
+	std::vector<std::string> result;
+	boost::split(result, input, boost::is_any_of(splitter));
+	return result;
+}
+
+
+bool compare_object_names_by_start_time(std::string object_name1, std::string object_name2) {
+	// Object name format is somehash-starttime-endtime
+	return std::stol(split_by_char(object_name1, "-")[1]) < std::stol(split_by_char(object_name2, "-")[1]);
 }
 
 std::vector<std::string> sort_object_names_on_start_time(std::vector<std::string> object_names) {
-	// TODO
+	sort(object_names.begin(), object_names.end(), compare_object_names_by_start_time);
+	return object_names;	
 }
 
-std::vector<std::string> get_trace_ids_with_index_status_code(std::string object_name, std::string status_code) {
-	// TODO
+std::vector<std::string> get_trace_ids_with_index_status_code(std::string object_name, std::string status_code, gcs::Client* client) {
+	auto object_content = read_object(TRACE_STRUCT_BUCKET, object_name, client);
+	std::vector<std::string> traces = split_by_string(object_content, "Trace ID: ");
+	std::vector<std::string> response;
+
+	for (auto current_trace : traces) {
+		if (true == does_trace_have_this_attribute(current_trace, object_name, trace_attribute::HTTP_STATUS_CODE, status_code, client)) {
+			response.push_back(split_by_char(current_trace, ":")[0]);
+		}
+	}
+
+	return response;
+}
+
+bool does_trace_have_this_attribute(
+	std::string trace_content, std::string batch_name, trace_attribute attr,
+	std::string attr_value, gcs::Client* client
+) {
+	auto trace_info = split_by_char(trace_content, "\n");
+
+	bool response = false;
+	for (int i = 1; i < trace_info.size(); i++) {
+		response = response || does_span_have_this_attribute(trace_info[i], batch_name, attr, attr_value, client);
+		if (true == response) {
+			break;
+		}
+	}
+
+	return response;
+}
+
+bool does_span_have_this_attribute(
+	std::string span_content, std::string batch_name, trace_attribute attr,
+	std::string attr_value, gcs::Client* client
+) {
+	auto span_info = split_by_char(span_content, ":");
+	auto span_id = span_info[1];
+	auto service_name = span_info[2];
+	return true;
 }
 
 batch_timestamp extract_batch_timestamps(std::string batch_name) {
@@ -66,10 +127,52 @@ batch_timestamp extract_batch_timestamps(std::string batch_name) {
 
 bool is_batch_big_enough(index_batch& current_index_batch) {
 	// TODO
+	return true;
 }
 
 int export_batch_to_storage(index_batch& current_index_batch, std::string index_status_code) {
 	// TODO
+	return 0;
+}
+
+std::string read_object(std::string bucket, std::string object, gcs::Client* client) {
+	auto reader = client->ReadObject(bucket, object);
+	if (!reader) {
+		std::cerr << "Error reading object " << bucket << "/" << object << " :" << reader.status() << "\n";
+		exit(1);
+	}
+
+	std::string object_content{std::istreambuf_iterator<char>{reader}, {}};
+	return object_content;
+}
+
+std::vector<std::string> split_by_string(std::string input, std::string splitter) {
+	std::vector<std::string> result;
+
+	size_t pos = 0;
+	std::string token;
+	while ((pos = input.find(splitter)) != std::string::npos) {
+		token = input.substr(0, pos);
+		token = strip_from_the_end(token, '\n');
+		if (token.length() > 0) {
+			result.push_back(token);
+		}
+		input.erase(0, pos + splitter.length());
+	}
+
+	input = strip_from_the_end(input, '\n');
+	if (input.length() > 0) {
+		result.push_back(input);
+	}
+
+	return result;
+}
+
+std::string strip_from_the_end(std::string object, char stripper) {
+	if (!object.empty() && object[object.length()-1] == stripper) {
+		object.erase(object.length()-1);
+	}
+	return object;
 }
 
 int dummy_tests() {
--- folders_index/status_code/status_code_index.h
+++ folders_index/status_code/status_code_index.h
@@ -19,6 +19,11 @@ const char TRACE_HASHES_BUCKET[] = "tracehashes-snicket4";
 const char SERVICES_BUCKETS_SUFFIX[] = "-snicket4";
 const char INDEX_STATUS_CODE_BUCKET[] = "index-status-code-snicket4";
 
+enum trace_attribute {
+    HTTP_STATUS_CODE,
+    IS_A_GOOD_TRACE  // just an example
+};
+
 struct batch_timestamp {
     std::string start_time;
     std::string end_time;
@@ -45,13 +50,27 @@ struct index_batch {
 namespace gcs = ::google::cloud::storage;
 using ::google::cloud::StatusOr;
 
+bool does_span_have_this_attribute(
+	std::string span_content, std::string batch_name, trace_attribute attr,
+	std::string attr_value, gcs::Client* client
+);
+bool does_trace_have_this_attribute(
+	std::string trace_content, std::string batch_name, trace_attribute attr,
+	std::string attribute_value, gcs::Client* client
+);
 int update_index(gcs::Client* client, time_t last_updated, std::string index_status_code);
 batch_timestamp extract_batch_timestamps(std::string batch_name);
-std::vector<std::string> get_all_object_names(std::string bucket_name);
+std::vector<std::string> get_all_object_names(std::string bucket_name, gcs::Client* client);
 std::vector<std::string> sort_object_names_on_start_time(std::vector<std::string> object_names);
-std::vector<std::string> get_trace_ids_with_index_status_code(std::string object_name, std::string status_code);
+std::vector<std::string> get_trace_ids_with_index_status_code(std::string object_name, std::string status_code, gcs::Client* client);
 bool is_batch_big_enough(index_batch& current_index_batch);
 int export_batch_to_storage(index_batch& current_index_batch, std::string index_status_code);
+std::vector<std::string> split_by_char(std::string input, std::string splitter);
+bool compare_object_names_by_start_time(std::string object_name1, std::string object_name2);
+std::string read_object(std::string bucket, std::string object, gcs::Client* client);
+std::vector<std::string> split_by_string(std::string input, std::string splitter);
+std::string strip_from_the_end(std::string object, char stripper);
+
 int dummy_tests();
 
 #endif // STATUS_CODE_INDEX_H_
