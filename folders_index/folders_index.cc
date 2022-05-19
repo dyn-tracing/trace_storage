5d9fa0617e3f769a4e25da071afe48d9877ce999
Muhammad Haseeb <mh6218@nyu.edu>
2022-05-19 15:32:23 -0400

    separated out main from folders index lib

--- folders_index/folders_index.cc
+++ folders_index/folders_index.cc
@@ -3,29 +3,6 @@
 
 #include "folders_index.h"
 
-int main(int argc, char* argv[]) {
-	dummy_tests();
-
-	auto client = gcs::Client();
-
-	std::string indexed_attribute = ATTR_SPAN_KIND;
-	create_index_bucket_if_not_present(indexed_attribute, &(client));
-	time_t last_updated = get_last_updated_for_bucket(get_bucket_name_for_attr(
-		indexed_attribute), &(client));
-
-	boost::posix_time::ptime start, stop;
-	start = boost::posix_time::microsec_clock::local_time();
-
-	update_index(&(client), last_updated, indexed_attribute);
-
-	stop = boost::posix_time::microsec_clock::local_time();
-
-	boost::posix_time::time_duration dur = stop - start;
-	int64_t milliseconds = dur.total_milliseconds();
-	std::cout << "Time taken: " << milliseconds << std::endl;
-	return 0;
-}
-
 void update_index_batched(gcs::Client* client, time_t last_updated, std::string indexed_attribute,
 	std::vector<std::string> span_buckets_names, std::vector<std::string>& trace_struct_object_names,
 	int batch_start_ind, int batch_size
---
+++ folders_index/main.cc
@@ -0,0 +1,24 @@
+#include "folders_index.h"
+
+int main(int argc, char* argv[]) {
+	dummy_tests();
+
+	auto client = gcs::Client();
+
+	std::string indexed_attribute = ATTR_SPAN_KIND;
+	create_index_bucket_if_not_present(indexed_attribute, &(client));
+	time_t last_updated = get_last_updated_for_bucket(get_bucket_name_for_attr(
+		indexed_attribute), &(client));
+
+	boost::posix_time::ptime start, stop;
+	start = boost::posix_time::microsec_clock::local_time();
+
+	update_index(&(client), last_updated, indexed_attribute);
+
+	stop = boost::posix_time::microsec_clock::local_time();
+
+	boost::posix_time::time_duration dur = stop - start;
+	int64_t milliseconds = dur.total_milliseconds();
+	std::cout << "Time taken: " << milliseconds << std::endl;
+	return 0;
+}
