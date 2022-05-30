39f90290cbe29acf469d968069ec9111a781601d
Muhammad Haseeb <mh6218@nyu.edu>
2022-05-30 11:56:13 -0400

    optimized filter_trace_ids_based_on_query_timestamp for structural query

--- graph_query_main.cc
+++ graph_query_main.cc
@@ -31,20 +31,27 @@ int main(int argc, char* argv[]) {
     // querying
     auto client = gcs::Client();
 
-    // auto total = get_traces_by_structure(query_trace, 1653317532, 1653317532, &client);
-    // std::cout << "Total traces: " << total.trace_ids.size() << std::endl;
-    // std::cout << "ID: " << total.trace_ids[0] << std::endl;
+    boost::posix_time::ptime start, stop;
+	start = boost::posix_time::microsec_clock::local_time();
 
-    return_value ret;
-    ret.node_index = 1;
-    ret.type = bytes_value;
-    get_value_func ret_union;
-    ret_union.bytes_func = &opentelemetry::proto::trace::v1::Span::parent_span_id;
+    auto total = get_traces_by_structure(query_trace, 1653919700, 1653919750, &client);
+    stop = boost::posix_time::microsec_clock::local_time();
+    boost::posix_time::time_duration dur = stop - start;
+	int64_t milliseconds = dur.total_milliseconds();
+	std::cout << "Time taken: " << milliseconds << std::endl;
+    std::cout << "Total traces: " << total.trace_ids.size() << std::endl;
+    std::cout << "ID: " << total.trace_ids[0] << std::endl;
 
-    ret.func = ret_union;
-    auto res = query(query_trace, 1653919700, 1653919900, conditions, ret, &client);
-    std::cout << "Total traces: " << res.size() << std::endl;
-    std::cout << "Example output: " << res[0] << std::endl;
+    // return_value ret;
+    // ret.node_index = 1;
+    // ret.type = bytes_value;
+    // get_value_func ret_union;
+    // ret_union.bytes_func = &opentelemetry::proto::trace::v1::Span::parent_span_id;
+
+    // ret.func = ret_union;
+    // auto res = query(query_trace, 1653919700, 1653919800, conditions, ret, &client);
+    // std::cout << "Total traces: " << res.size() << std::endl;
+    // std::cout << "Example output: " << res[0] << std::endl;
 
     // std::string batch = query_bloom_index_for_value(&client, "c5367e16e960a3452529e44d035a9bec", "new_id_index");
     // std::cout << "batch " << batch << std::endl;
