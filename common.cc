2c1199eb19498637a715b2e857de028c9fdf27ef
Jessica Berg <jesswberg@gmail.com>
2022-05-30 11:28:17 -0500

    lint

--- common.cc
+++ common.cc
@@ -277,9 +277,13 @@ void print_progress(float progress, std::string label, bool verbose) {
     std::cout << " [";
     int pos = barWidth * progress;
     for (int i = 0; i < barWidth; ++i) {
-        if (i < pos) std::cout << "=";
-        else if (i == pos) std::cout << ">";
-        else std::cout << " ";
+        if (i < pos) {
+            std::cout << "=";
+        } else if (i == pos) {
+            std::cout << ">";
+        } else {
+            std::cout << " ";
+        }
     }
     std::cout << "] " << int(progress * 100.0) << "% " << label << "\r";
     std::cout.flush();
