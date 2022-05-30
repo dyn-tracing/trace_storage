9d53b4b20105c13946a2224dcc83f1ca01284fd7
Jessica Berg <jesswberg@gmail.com>
2022-05-30 13:32:53 -0500

    now everything in graph query is pass by ref rather than val

--- graph_query.cc
+++ graph_query.cc
@@ -112,7 +112,7 @@ std::tuple<objname_to_matching_trace_ids, std::map<std::string, iso_to_span_id>>
     traces_by_structure &structural_results,
     std::vector<query_condition> &conditions,
     struct fetched_data &fetched,
-    return_value ret
+    return_value &ret
 ) {
     objname_to_matching_trace_ids to_return_traces;
     std::map<std::string, iso_to_span_id> trace_id_to_span_id_mappings;
@@ -131,7 +131,7 @@ std::tuple<objname_to_matching_trace_ids, std::map<std::string, iso_to_span_id>>
 }
 
 objname_to_matching_trace_ids intersect_index_results(
-    std::vector<objname_to_matching_trace_ids> index_results,
+    std::vector<objname_to_matching_trace_ids> &index_results,
     traces_by_structure &structural_results, bool verbose) {
     // Easiest solution is just keep a count
     // Eventually we should parallelize this, but I'm not optimizing it
@@ -192,12 +192,12 @@ std::string get_return_value_from_traces_data(
             return get_value_as_string(sp, ret.func, ret.type);
         }
     }
-    std::cerr << "didn't find the span " << span_to_find << " I was looking for " << std::endl << std::flush;
+    //std::cerr << "didn't find the span " << span_to_find << " I was looking for " << std::endl << std::flush;
     return "";
 }
 std::vector<std::string> get_return_value(
     std::tuple<objname_to_matching_trace_ids, std::map<std::string, iso_to_span_id>> &filtered,
-    return_value ret, fetched_data &data, trace_structure &query_trace, gcs::Client* client) {
+    return_value &ret, fetched_data &data, trace_structure &query_trace, gcs::Client* client) {
     std::vector<std::string> to_return;
 
     for (auto const &obj_to_trace_ids : std::get<0>(filtered)) {
@@ -278,9 +278,10 @@ fetched_data fetch_data(
     return response;
 }
 
-std::map<int, std::map<int, std::string>> does_trace_satisfy_conditions(std::string trace_id, std::string object_name,
+std::map<int, std::map<int, std::string>> does_trace_satisfy_conditions(
+    const std::string &trace_id, const std::string &object_name,
     std::vector<query_condition> &conditions, fetched_data& evaluation_data,
-    traces_by_structure &structural_results, return_value ret
+    traces_by_structure &structural_results, return_value& ret
 ) {
     // isomap_index_to_node_index_to_span_id -> ii_to_ni_to_si
     std::vector<std::map<int, std::map<int, std::string>>> ii_to_ni_to_si_data_for_all_conditions;
@@ -329,8 +330,9 @@ std::string get_service_name_for_node_index(
 }
 
 std::map<int, std::map<int, std::string>> get_iso_maps_indices_for_which_trace_satifies_curr_condition(
-    std::string trace_id, std::string batch_name, std::vector<query_condition>& conditions,
-    int curr_cond_ind, fetched_data& evaluation_data, traces_by_structure& structural_results, return_value ret
+    const std::string &trace_id, const std::string &batch_name,
+    std::vector<query_condition>& conditions,
+    int curr_cond_ind, fetched_data& evaluation_data, traces_by_structure& structural_results, return_value& ret
 ) {
     std::map<int, std::map<int, std::string>> response;
 
@@ -377,8 +379,8 @@ std::map<int, std::map<int, std::string>> get_iso_maps_indices_for_which_trace_s
 }
 
 bool does_span_satisfy_condition(
-    std::string span_id, std::string service_name,
-    query_condition condition, std::string batch_name, fetched_data& evaluation_data
+    std::string &span_id, std::string &service_name,
+    query_condition &condition, const std::string &batch_name, fetched_data& evaluation_data
 ) {
     if (evaluation_data.spans_objects_by_bn_sn.find(batch_name) == evaluation_data.spans_objects_by_bn_sn.end()
     || evaluation_data.spans_objects_by_bn_sn[batch_name].find(
