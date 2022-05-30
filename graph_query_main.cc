9f30134c49d89734f0687cec83d91379c0a437a0
Muhammad Haseeb <mh6218@nyu.edu>
2022-05-30 10:53:35 -0400

    changed query

--- graph_query_main.cc
+++ graph_query_main.cc
@@ -5,8 +5,8 @@
 int main(int argc, char* argv[]) {
     // query trace structure
     trace_structure query_trace;
-    query_trace.num_nodes = 1;
-    query_trace.node_names.insert(std::make_pair(0, "currencyservice"));
+    query_trace.num_nodes = 3;
+    query_trace.node_names.insert(std::make_pair(0, "frontend"));
     query_trace.node_names.insert(std::make_pair(1, "adservice"));
     query_trace.node_names.insert(std::make_pair(2, ASTERISK_SERVICE));
 
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]) {
     ret_union.bytes_func = &opentelemetry::proto::trace::v1::Span::parent_span_id;
 
     ret.func = ret_union;
-    auto res = query(query_trace, 1653317532, 1653317532, conditions, ret, &client);
+    auto res = query(query_trace, 1653919700, 1653919900, conditions, ret, &client);
     std::cout << "Total traces: " << res.size() << std::endl;
     std::cout << "Example output: " << res[0] << std::endl;
 
