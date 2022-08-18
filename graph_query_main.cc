a967bc024e85ec03642a3b7efc4d471d3969ab01
Muhammad Haseeb <mh6218@nyu.edu>
2022-08-18 10:55:26 -0400

    trying to run query with trace id condition

--- graph_query_main.cc
+++ graph_query_main.cc
@@ -5,28 +5,28 @@
 int main(int argc, char* argv[]) {
     // query trace structure
     trace_structure query_trace;
-    query_trace.num_nodes = 3;
-    query_trace.node_names.insert(std::make_pair(0, "frontend"));
-    query_trace.node_names.insert(std::make_pair(1, "adservice"));
-    query_trace.node_names.insert(std::make_pair(2, ASTERISK_SERVICE));
+    query_trace.num_nodes = 1;
+    query_trace.node_names.insert(std::make_pair(0, ASTERISK_SERVICE));
+    // query_trace.node_names.insert(std::make_pair(1, "adservice"));
+    // query_trace.node_names.insert(std::make_pair(2, ASTERISK_SERVICE));
 
-    query_trace.edges.insert(std::make_pair(0, 1));
-    query_trace.edges.insert(std::make_pair(1, 2));
+    // query_trace.edges.insert(std::make_pair(0, 1));
+    // query_trace.edges.insert(std::make_pair(1, 2));
 
     // query conditions
     std::vector<query_condition> conditions;
 
-    // query_condition condition1;
-    // condition1.node_index = 2;
-    // condition1.type = int_value;
-    // get_value_func condition_1_union;
-    // condition_1_union.int_func = &opentelemetry::proto::trace::v1::Span::start_time_unix_nano;
-    // condition1.func = condition_1_union;
-    // condition1.node_property_value = "10000000";  // 1e+7 ns, 10 ms
-    // condition1.comp = Lesser_than;
-    // condition1.property_name = "starttime";
+    query_condition condition1;
+    condition1.node_index = 0;
+    condition1.type = bytes_value;
+    get_value_func condition_1_union;
+    condition_1_union.bytes_func = &opentelemetry::proto::trace::v1::Span::trace_id;
+    condition1.func = condition_1_union;
+    condition1.node_property_value = "b3c71ca478842867efba41a4eccfbed0";
+    condition1.comp = Equal_to;
+    condition1.property_name = "trace-id";
 
-    // conditions.push_back(condition1);
+    conditions.push_back(condition1);
 
     // querying
     auto client = gcs::Client();
@@ -41,8 +41,8 @@ int main(int argc, char* argv[]) {
     boost::posix_time::ptime start, stop;
 	start = boost::posix_time::microsec_clock::local_time();
     
-    // auto res = query(query_trace, 1653919700, 1653919710, conditions, ret, true, &client);
-    auto res2 = get_traces_by_structure(query_trace, 1660239561, 1660239571, &client);
+    auto res = query(query_trace, 1660674000, 1660676000, conditions, ret, true, &client);
+    // auto res2 = get_traces_by_structure(query_trace, 1660239561, 1660239571, &client);
     
     stop = boost::posix_time::microsec_clock::local_time();
     boost::posix_time::time_duration dur = stop - start;
