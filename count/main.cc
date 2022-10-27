35e608b568adc613a3b1e34943637a4204a70488
jessberg <jessberg@users.noreply.github.com>
2022-10-27 17:43:55 -0400

    Count it (#54)
    * merge together all counting code
    
    Co-authored-by: Muhammad Haseeb <mh6218@nyu.edu>

---
+++ count/main.cc
@@ -0,0 +1,11 @@
+#include "count_traces.h"
+#include "count_objects.h"
+
+int main(int argc, char* argv[]) {
+    auto client = gcs::Client();
+    count_spans_and_traces(&client);
+    const int64_t res = count_objects(&client, true);
+    std::cout << "Total data size " << res << std::endl;
+    const int64_t res2 = count_objects(&client, false);
+    std::cout << "Total objects " << res2 << std::endl;
+}
