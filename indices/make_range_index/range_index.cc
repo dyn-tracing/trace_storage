adcbe52f7177d32779d2e804477cf1bc664fe6e6
Jessica Berg <jesswberg@gmail.com>
2022-11-14 16:00:57 -0500

    update

--- indices/make_range_index/range_index.cc
+++ indices/make_range_index/range_index.cc
@@ -1,75 +1,46 @@
 
 #include "range_index.h"
 
-void Serialize(std::ostream &os, Node &node) {
-    os.write((char *) &node.start_time, sizeof(time_t));
-    os.write((char *) &node.end_time, sizeof(time_t));
-    os.write((char *) &node.start_value, sizeof(int64_t));
-    os.write((char *) &node.end_value, sizeof(int64_t));
-    size_t size = node.values.size();
-    os.write((char *) &size, sizeof(size_t));
-    for (unsigned int i=0; i < node.values.size(); i++) {
-        os.write((char *) &node.values[i], sizeof(int64_t));
-    }
+void Node::Serialize(std::ostream &os) {
+    os.write((char *) &start_time, sizeof(time_t));
+    os.write((char *) &end_time, sizeof(time_t));
 }
 
-Node DeserializeNode(std::istream &is){
-    Node node;
-    is.read((char *) &node.start_time, sizeof(time_t));
-    is.read((char *) &node.end_time, sizeof(time_t));
-    is.read((char *) &node.start_value, sizeof(int64_t));
-    is.read((char *) &node.end_value, sizeof(int64_t));
-    size_t values_size;
-    is.read((char *) &values_size, sizeof(size_t));
-    node.values.reserve(values_size);
-    for (unsigned int i=0; i<values_size; i++) {
-        int64_t val;
-        is.read((char *) &val, sizeof(int64_t));
-        node.values.push_back(val);
-    }
-    return node;
+Status Node::Deserialize(std::istream &is) {
+    is.read(reinterpret_cast<char *>(&start_time), sizeof(time_t));
+    is.read((char *) &end_time, sizeof(time_t));
+    return Status();
 }
 
-void Serialize(std::ostream &os, NodeSummary &sum){
-    os.write((char *) &sum.start_time, sizeof(time_t));
-    os.write((char *) &sum.end_time, sizeof(time_t));
-    size_t size = sum.node_objects.size();
-    os.write((char *) &size, sizeof(size_t));
-    for (unsigned int i=0; i<size; i++) {
-        os.write((char *) &sum.node_objects[i], sizeof(int64_t));
-    }
+void NodeSummary::Serialize(std::ostream &os){
+    os.write((char *) &start_time, sizeof(time_t));
+    os.write((char *) &end_time, sizeof(time_t));
 }
 
-NodeSummary DeserializeNodeSummary(std::istream &is) {
+Status NodeSummary::Deserialize(std::istream &is) {
     NodeSummary sum;
     is.read((char *) &sum.start_time, sizeof(time_t));
     is.read((char *) &sum.end_time, sizeof(time_t));
-    size_t size;
-    is.read((char *) &size, sizeof(size_t));
-    sum.node_objects.reserve(size);
-    for (unsigned int i=0; i<size; i++) {
-        is.read((char *) &sum.node_objects[i], sizeof(int64_t));
-    }
-    return sum;
+    return Status();
 }
 
 Status get_last_updated_and_granularity(
     gcs::Client* client, time_t &granularity, time_t &last_updated,
     std::string index_bucket
 ) {
-    StatusOr<gcs::BucketMetadata> bucket_metadata =                             
-      client->GetBucketMetadata(index_bucket);                                            
+    StatusOr<gcs::BucketMetadata> bucket_metadata =
+      client->GetBucketMetadata(index_bucket);
     if (!bucket_metadata) {
-        throw std::runtime_error(bucket_metadata.status().message());           
-        return bucket_metadata.status();                                        
-    }                                                                           
-    for (auto const& kv : bucket_metadata->labels()) {                          
+        throw std::runtime_error(bucket_metadata.status().message());
+        return bucket_metadata.status();
+    }
+    for (auto const& kv : bucket_metadata->labels()) {
         if (kv.first == "last_updated") {
             last_updated = time_t_from_string(kv.second);
         }
-        if (kv.first == "granularity") {                                        
-            granularity = time_t_from_string(kv.second);                        
-        }                                                                       
+        if (kv.first == "granularity") {
+            granularity = time_t_from_string(kv.second);
+        }
     }
     return Status();
 }
@@ -84,35 +55,36 @@ StatusOr<std::vector<RawData>> retrieve_single_batch_data(
         TRACE_STRUCT_BUCKET, batches[batch_index], client);
     const opentelemetry::proto::trace::v1::Span* sp;
 
-    for (int i=0; i < trace_data.resource_spans(0).scope_spans(0).spans_size(); i++) {
-        sp = &(trace_data.resource_spans(0).scope_spans(0).spans(i));           
-                                                                                
+    for (int i=0;
+        i < trace_data.resource_spans(0).scope_spans(0).spans_size();
+        i++) {
+        sp = &(trace_data.resource_spans(0).scope_spans(0).spans(i));
         std::string trace_id = hex_str(sp->trace_id(), sp->trace_id().length());
         time_t timestamp = sp->start_time_unix_nano();
-                                                                                
-        const opentelemetry::proto::common::v1::KeyValue* attribute;            
-        for (int j=0; j < sp->attributes_size(); j++) {                         
-            attribute =  &(sp->attributes(j));                                  
-            const opentelemetry::proto::common::v1::AnyValue* val = &(attribute->value());
-            auto curr_attr_key = attribute->key();                              
-            std::string curr_attr_val = "";                                     
-                                                                                
-            switch (val->value_case()) {                                        
-            case 1:                                                             
-                curr_attr_val = val->string_value();                            
-                break;                                                          
-            case 2:                                                             
-                curr_attr_val = val->bool_value() ? "true" : "false";           
-                break;                                                          
-            case 3:                                                             
-                curr_attr_val = std::to_string(val->int_value());               
-                break;                                                          
-            case 4:                                                             
-                curr_attr_val = std::to_string(val->double_value());            
-                break;                                                          
-            default:      
+        const opentelemetry::proto::common::v1::KeyValue* attribute;
+        for (int j=0; j < sp->attributes_size(); j++) {
+            attribute =  &(sp->attributes(j));
+            const opentelemetry::proto::common::v1::AnyValue* val =
+                &(attribute->value());
+            auto curr_attr_key = attribute->key();
+            std::string curr_attr_val = "";
+            switch (val->value_case()) {
+            case 1:
+                curr_attr_val = val->string_value();
+                break;
+            case 2:
+                curr_attr_val = val->bool_value() ? "true" : "false";
+                break;
+            case 3:
+                curr_attr_val = std::to_string(val->int_value());
+                break;
+            case 4:
+                curr_attr_val = std::to_string(val->double_value());
+                break;
+            default:
                 return Status(
-                    ::google::cloud::StatusCode::kUnavailable, "could not translate attribute");
+                    ::google::cloud::StatusCode::kUnavailable,
+                    "could not translate attribute");
                 break;
             }
             if (attribute_to_index == curr_attr_key) {
@@ -130,27 +102,29 @@ StatusOr<std::vector<RawData>> retrieve_single_batch_data(
     return to_return;
 }
 
-StatusOr<std::vector<RawData>> retrieve_data(
+StatusOr<std::vector<RawData>> organize_data_into_nodes(
     const std::vector<std::string> &batches,
     const std::string attribute_to_index,
+    std::map<time_t, Node> nodes,
     gcs::Client* client) {
     std::vector<std::future<StatusOr<std::vector<RawData>>>> data_futures;
     data_futures.reserve(batches.size());
-    for (int64_t i=0; i<batches.size(); i++) {
+    for (int64_t i=0; i < batches.size(); i++) {
         data_futures.push_back(std::async(std::launch::async,
-            retrieve_single_batch_data, batches, i, attribute_to_index, client));
+            retrieve_single_batch_data,
+            batches, i, attribute_to_index, client));
     }
-    std::vector<RawData> all_data;
-    for (int64_t i=0; i<data_futures.size(); i++) {
+
+    for (int64_t i=0; i < data_futures.size(); i++) {
         StatusOr<std::vector<RawData>> partial_data = data_futures[i].get();
         if (!partial_data.ok()) {
             return partial_data;
         }
-        all_data.insert(all_data.end(),
-                        partial_data->begin(),
-                        partial_data->end());
+        // for each piece of raw data, integrate into Nodes
+        for (int64_t j = 0; j < partial_data->size(); j++) {
+            // TODO
+        }
     }
-    return all_data;
 }
 
 Status update(std::string indexed_attribute, gcs::Client* client) {
@@ -159,19 +133,43 @@ Status update(std::string indexed_attribute, gcs::Client* client) {
 
     // 1. Find granularity and last updated
     time_t granularity, last_updated;
-    Status ret = get_last_updated_and_granularity(client, granularity, last_updated, index_bucket);
+    Status ret = get_last_updated_and_granularity(
+        client, granularity, last_updated, index_bucket);
     if (!ret.ok()) {
         return ret;
     }
 
-    // 2. Find all objects that have happened since last updated
+    // 2. Create nodes to be added.
+    std::map<time_t, Node> nodes;
     time_t now;
     time(&now);
-    std::vector<std::string> batches = get_batches_between_timestamps(client, last_updated, now);
+    now = now - (now % TIME_RANGE_PER_NODE);
+
+    for (time_t i = last_updated; i < now; i += TIME_RANGE_PER_NODE) {
+       nodes[i] = Node{
+        .start_time = i,
+        .end_time = i + TIME_RANGE_PER_NODE,
+       };
+    }
+
+    // 3. Find all objects that have happened since last updated
+    // We need to update from now - time range per node left
+    std::vector<std::string> batches = get_batches_between_timestamps(
+        client, last_updated, now);
 
     // 3. Organize their data into summary and node objects
-    //    To do so, we need to know (1) batch name, (2) timestamp, (3) trace ID, (4) data
-    StatusOr<std::vector<RawData>> data = retrieve_data(batches, indexed_attribute, client);
+    //    To do so, we need to know (1) batch name, (2) timestamp,
+    //    (3) trace ID, (4) data
+    StatusOr<std::vector<RawData>> data = organize_data_into_nodes(
+        batches, indexed_attribute, nodes, client);
+    if (!data.ok()) {
+        return data.status();
+    }
+
+    // Organize. Make nodes for all timestamps
+    // From last_updated to now - other bits.
+    // Because we modded now to TIME_RANGE_PER_NODE, we know that it
+    // divides evenly
 
     // 4. Send that information to GCS.
 }
--- indices/make_range_index/range_index.h
+++ indices/make_range_index/range_index.h
@@ -7,18 +7,50 @@
 const int64_t TIME_RANGE_PER_NODE = 10;
 const int64_t  NUM_NODES_PER_SUMMARY = 20;
 
+struct IndexedData {
+    std::string batch_name;
+    std::string trace_id;
+    int64_t data;
+
+    void Serialize(std::ostream &os);
+    Status Deserialize(std::istream &is);
+};
+
+struct NodePieces {
+    int64_t start_value;
+    int64_t end_value;
+    std::vector<IndexedData> values;
+
+    void Serialize(std::ostream &os);
+    Status Deserialize(std::istream &is);
+};
+
 struct Node {
     time_t start_time;
     time_t end_time;
-    int64_t start_value;
-    int64_t end_value;
-    std::vector<int64_t> values;
+    // Each piece should be roughly 1 GB.
+    std::vector<NodePieces> node_pieces;
+
+    void Serialize(std::ostream &os);
+    Status Deserialize(std::istream &is);
+};
+
+struct NodeID {
+    time_t start_time;
+    time_t end_time;
+    int64_t num_node_pieces;
+
+    void Serialize(std::ostream &os);
+    Status Deserialize(std::istream &is);
 };
 
 struct NodeSummary {
     time_t start_time;
     time_t end_time;
     std::vector<int64_t> node_objects;
+
+    void Serialize(std::ostream &os);
+    Status Deserialize(std::istream &is);
 };
 
 struct RawData {
@@ -27,10 +59,5 @@ struct RawData {
     std::string trace_id;
     std::string data;
 };
-
-void Serialize(std::ostream &os, Node &node);
-Node DeserializeNode(std::istream &is);
-void Serialize(std::ostream &os, NodeSummary &sum);
-NodeSummary DeserializeNodeSummary(std::istream &is);
 
 Status update(std::string indexed_attribute, gcs::Client* client);
